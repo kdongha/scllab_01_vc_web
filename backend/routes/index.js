ab131b3f405248ba56384b208f5f90b9cf65ed5a
Dongha <kdongha1597@gmail.com>
2018-09-28 00:35:05 +0900

    add restAPI(/targetList, /remove)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var targetPath = './target';
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const getDirs = (path) => fs.readdirSync(path).filter(file => {
+    return fs.statSync(path + '/' + file).isDirectory();
+});
+
+router.get('/targetList', function (req, res, next) {
+    res.json({'targetList': getDirs(targetPath)})
+});
+
+router.get('/remove', function (req, res, next) {
+    const removeTarget = req.query['target'];
+    fs.rmdir(targetPath + '/' + removeTarget, (err, stats) => {
+        res.json({'targetList': getDirs(targetPath)});
+    });
+});
+
+router.get('/train', function (req, res, next) {
+
+});
+
+router.get('/conversion', function (req, res, next) {
+
 });
 
 module.exports = router;
